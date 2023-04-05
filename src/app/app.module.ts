f45df4cdbf2f7d45519b163118e7a75fe8baa4f1
ghoshshreya <shrghosh08@gmail.com>
2023-04-05 14:16:38 +0530

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
-import { CustomNgIfDirective } from "./custom-ng-if.directive";
-import { UppercaseDirective } from './uppercase.directive';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { CustomNgIfDirective } from './custom-ng-if.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, CustomNgIfDirective, UppercaseDirective],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HelloComponent, CustomNgIfDirective],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
