10cf98a62ff048609218ab5e2e69971b7b0ae9cb
ghoshshreya <shrghosh08@gmail.com>
2023-04-11 21:47:42 +0530

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { CustomNgIfDirective } from './custom-ng-if.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, CustomNgIfDirective],
+  declarations: [AppComponent, CustomNgIfDirective],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
